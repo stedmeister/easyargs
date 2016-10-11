eaf676c02c3699e66201a5510e1dd635405d9e2c
Steddy Smit <jacob.smit@zepler.net>
2016-10-11 08:51:39 +0100

    Added the package's modules as properties so that imports work better

--- easyargs/__init__.py
+++ easyargs/__init__.py
@@ -1,5 +1,10 @@
 import decorators
+import parsers
 import sys
 
 # This bit of magic allows us to use the module name as a decorator
+decorators.make_easy_args.parsers = parsers
+decorators.make_easy_args.decorators = decorators
+
 sys.modules[__name__] = decorators.make_easy_args
+
