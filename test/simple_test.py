cfeb1b844945e643f891f9cc14a9eff0ac95e92b
Steddy Smit <jsmit@cisco.com>
2016-10-11 14:32:18 +0100

    Used print as a function

--- test/simple_test.py
+++ test/simple_test.py
@@ -1,10 +1,12 @@
+from __future__ import print_function
+
 import easyargs
 
 @easyargs
 def main(name, count=1, greeting='Hello'):
     """A simple greeting program"""
     for i in range(count):
-        print '{greeting} {name}!'.format(greeting=greeting, name=name)
+        print ('{greeting} {name}!'.format(greeting=greeting, name=name))
 
 
 if __name__ == '__main__':
